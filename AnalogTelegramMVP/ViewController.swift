0af1b94f7fc1fc7b5cb45b5f9dd737f09af3e5cd
Ilia <ilya2491@gmail.com>
2023-08-28 16:13:53 +0400

    Initial Commit

---
+++ AnalogTelegramMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnalogTelegramMVP
+//
+//  Created by i0240 on 28.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
